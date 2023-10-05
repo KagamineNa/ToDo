bf54d205d550ce1f412a7d3a25da0a4dfc41947e
Nguyen Ngoc Anh <nguyenngocanh2622003@gmail.com>
2023-10-06 01:12:21 +0700

    finish

--- todos/urls.py
+++ todos/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('addTask/', views.addTask, name='addTask')  
+    path('addTask/', views.addTask, name='addTask'),
+    path('markAsDone/<int:pk>/', views.markAsDone, name = 'markAsDone'),
+    path('deleteTask/<int:pk>/', views.deleteTask, name = 'deleteTask'),
+    path('markAsUndone/<int:pk>/', views.markAsUndone, name = 'markAsUndone'),
+    path('editTask/<int:pk>/', views.editTask, name = 'editTask')
 ]
 
 # 12700.8000/todo/addTask/
